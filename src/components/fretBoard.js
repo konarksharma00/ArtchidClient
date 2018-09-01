0702cd3fdf5eea0fc15ceeb0664cb36d26aaefe3
konark <konarksharma00@gmail.com>
2018-09-01 09:44:54 -0700

    git cache removed

--- src/components/fretBoard.js
+++ src/components/fretBoard.js
@@ -3,21 +3,10 @@ import Fertboard from '../assets/images/fretboard.png';
 
 const FretBoard = function () {
     return (
-        <div className="col-xs-12">
-            <span className="col-xs-12 brand-logo">
+        <div className="col-xs-12 img-fluid">
+            <span className="col-xs-12">
                 <img src={Fertboard} alt="logo" />
             </span>
-            <div className="col-xs-12 introHeader">
-                Who will win
-            </div>
-            <div className="col-xs-12">
-                <div className="col-xs-6">
-                    Shann
-                </div>
-                <div className="col-xs-6">
-                    Sonu Nigam
-                </div>
-            </div>
         </div>
     )
 }
